266ba44cc4751e3e1f1d42537cfef28dfab30a70
charlesCXK <715811763@qq.com>
2018-06-03 11:11:53 +0800

    修改了数据库中comment的结构 && 修复了res返回值json的问题

--- src/server/api/userapi.js
+++ src/server/api/userapi.js
@@ -333,6 +333,8 @@ router.use('/getPostByID', function (req, res) {
 	console.log("********************************* getPostByID *********************************");
 	console.log("GET a postID : " + postid);
 
+
+
 	var insertobj={};		// 要插入的表项
 
 	// 连接数据库
@@ -340,34 +342,31 @@ router.use('/getPostByID', function (req, res) {
 	    if (err) throw err;
 	    console.log('数据库已连接');
 	    var dbo = db.db("filesharer");
-	    
-	    res.json({
-	    	code: 0,// 0 for success, 1 for error
-		    post: {
-		        // everything about the post in the database
-		    }
-    	})
 
 		dbo.collection("bbs").find({bbs_id:postid}).toArray(function(err, ress) {
 			if (err) throw err;
+			var returnobj = {}
+			var retcode = 1;
 
 			if(ress.length == 0){
-				res['code'] = 0;
+				retcode = 1;
 			}
 			else{
-				var returnobj = {"bbs_id":postid, "username":ress[0]['username'], "date":ress[0]['date'], "title":ress[0]['title'], "content":ress[0]['content'], "comment":ress[0]['comment'], "comment_user": ress[0]['comment_user']};
+				returnobj = {"bbs_id":postid, "username":ress[0]['username'], "date":ress[0]['date'], "title":ress[0]['title'], "content":ress[0]['content'], "comment":ress[0]['comment']};
 				console.log('帖子信息加载完毕');
 
-				res['post'] = returnobj;	
+				retcode = 0;
 			}
+
+			res.json({
+		    	code: retcode,// 0 for success, 1 for error
+			    post: returnobj
+			})
 
 			console.log(res['post']);
 
-
-			db.close();
-		});
-		console.log(res['post']);
-		
+		});		
+		db.close();
 	});
 
 })
